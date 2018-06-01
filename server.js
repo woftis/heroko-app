af5f23dbb812638d7f150fe8ff21cbbe3a046170
Steven Walker <stevenwalker1991@googlemail.com>
2018-06-01 11:30:44 +0100

    make it better

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const port = 8000;
+
+app.listen(port, function() {
+  console.log("We're live on " + port);
+})
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
